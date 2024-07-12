33513cc6592b9714c8857363db0636600c9f8111
Gonzalo <elias.gonzaloguerrero@gmail.com>
2024-07-11 21:56:05 -0400

    added api call to rendering images with names

---
+++ src/API/data.jsx
@@ -0,0 +1,6 @@
+/* const requesApi = () => {
+  fetch('https://dragonball-api.com/api/characters?page=2&limit=10').then(
+    (data) => console.log(data.json())
+  )
+}
+ */
--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,23 @@
+import { useState, useEffect } from 'react'
+import { v4 as uuidv4 } from 'uuid'
 export const App = () => {
+  const [data, setData] = useState('')
+  useEffect(() => {
+    fetch('https://dragonball-api.com/api/characters?page=2&limit=10')
+      .then((resp) => (resp.json()))
+      .then((data) => setData(data.items))
+  }, [])
   return (
     <>
       <main>
-        <h1>hello world</h1>
+        {data &&
+          data.map((fight) => (
+            <div className='card' key={uuidv4()}>
+              <h1>{fight.name}</h1>
+              <img src={fight.image} alt="" />
+            </div>
+          ))}
       </main>
     </>
-  );
-};
+  )
+}
